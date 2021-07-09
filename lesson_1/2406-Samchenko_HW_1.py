688a3ecd1e8a09500cf928725e550914ce1b44e0
Max Samchenko <samchenkoms@gmail.com>
2021-07-09 14:24:41 +0300

    Edit HW 1 lines feed

--- lesson_1/2406-Samchenko_HW_1.py
+++ lesson_1/2406-Samchenko_HW_1.py
@@ -3,13 +3,16 @@
 # Task 1: 
 # Create a script that can calculate squares of the next geometric figures:
 # circle, rectangle triangle(with an angle 90 degrees), rectangle
-r = 10      # here you should set the raduis of the circle
+r = 10      # here you should set the radius of the circle
 a = 15      # here you should set the first side of the rectangle
 b = 12      # here you should set the second side of the rectangle
 
-circle_square = 3.14 * (r ** 2)         # here you should define a formula of the circle
-rec_triangle_square = (a * b) / 2       # here you should define a formula of the rectangle triangle
-rectangle_square = a * b                # here you should define a formula of the rectangle
+circle_square = 3.14 * (r ** 2)
+# here you should define a formula of the circle
+rec_triangle_square = (a * b) / 2
+# here you should define a formula of the rectangle triangle
+rectangle_square = a * b
+# here you should define a formula of the rectangle
 
 print(f'Circle: r={r}, S={circle_square}')
 print(f'Rectangle triangle: a={a}, b={b}, S={rec_triangle_square}')
